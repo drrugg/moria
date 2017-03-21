795c33395e52a91b9e093ee90cba55fd14d28a98
drrugg <drrugg+github@gmail.com>
2017-03-21 13:15:44 +1300

    Update markers.js
    reducing and shifting examples out

--- markers.js
+++ markers.js
@@ -8,34 +8,14 @@
 var MAPCRAFTER_MARKERS = [
     // just one example marker group
     {
-        // id of the marker group, without spaces/other special chars
         "id" : "signs",
-        // name of the marker group, displayed in the webinterface
         "name" : "Signs",
-        // icon of the markers belonging to that group (optional)
         "icon" : "sign.png",
-        // size of that icon
         "iconSize" : [32, 32],
-        // whether this marker group is shown by default (optional)
         "showDefault" : true,
-        // markers of this marker group...
+
         "markers" : {
-            // ...in the world "world"
             "world" : [
-                // example marker, pretty format:
-                {
-                    // position ([x, z, y])
-                    "pos" : [35, -21, 64],
-                    // title when you hover over the marker
-                    "title" : "Sign1",
-                    // text in the marker popup window
-                    "text" : "Hello."
-                    // override the icon of a single marker (optional)
-                    "icon" : "player.png",
-                    // override the size of the marker icon (optional)
-                    "iconSize" : [16, 32]
-                },
-                // more markers:
                 {"pos" : [100, 100, 64], "title" : "Test1"},
                 {"pos" : [100, 200, 64], "title" : "Test2"},
                 {"pos" : [500, 30, 64], "title" : "Test2"},
@@ -58,4 +38,34 @@ var MAPCRAFTER_MARKERS = [
             ],
         },
     },
+    
 ];
+
+
+// Marker Example
+//{
+//        "id" : "signs",        // id of the marker group, without spaces/other special chars
+//        "name" : "Signs",        // name of the marker group, displayed in the webinterface
+//        "icon" : "sign.png",        // icon of the markers belonging to that group (optional)
+//        "iconSize" : [32, 32],       // size of that icon
+//        "showDefault" : true,        // whether this marker group is shown by default (optional)
+//
+//        // markers of this marker group...
+//        "markers" : {
+//            // ...in the world "world":[],"world2":[] etc
+//            "world" : [
+//                {                           // example marker, pretty format:
+//                    "pos" : [35, -21, 64],  // position ([x, z, y])
+//                    "title" : "Sign1",      // title when you hover over the marker
+//                    "text" : "Hello."       // text in the marker popup window
+//                    "icon" : "player.png",  // override the icon of a single marker (optional)
+//                    "iconSize" : [16, 32]   // override the size of the marker icon (optional)
+//                },
+//
+//                // more markers:
+//                {"pos" : [100, 100, 64], "title" : "Test1"},
+//                {"pos" : [100, 200, 64], "title" : "Test2"},
+//                {"pos" : [500, 30, 64], "title" : "Test2"},
+//            ],
+//        },
+//    }
